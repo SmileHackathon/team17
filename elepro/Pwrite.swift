4b526092829ba6a915496fecc555c9daeaab05e2
znrysk <sincostan0420@gmail.com>
2021-12-13 21:11:06 +0900

    first commit to fix

---
+++ elepro/Pwrite.swift
@@ -0,0 +1,15 @@
+//
+//  Pwrite.swift
+//  elepro
+//
+//  Created by enPiT2016MBP-01 on 2021/12/13.
+//
+
+import SwiftUI
+
+struct Pwrite: View {//頼まれたプロフを記入するところ
+    var body: some View{
+        Text("Pwrite").font(.title)
+        ButtonBottom()
+    }
+}
