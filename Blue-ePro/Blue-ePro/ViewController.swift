d6782c0606e61911f6a7396fc31965311871f7ae
enPiT2016MBP-01 <codepro@enPiT2016MBP-01.local>
2021-12-16 00:59:22 +0900

    first

---
+++ Blue-ePro/Blue-ePro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Blue-ePro
+//
+//  Created by enPiT2016MBP-01 on 2021/12/14.
+//
+
+import UIKit
+//import Accounts
+//import MultipeerConnectivity
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
